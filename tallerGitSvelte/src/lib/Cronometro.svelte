544a5ca09211dba672107bab3ad5e627dea2bf4d
Hugo Sanz <148755800+ggdsrll@users.noreply.github.com>
2024-11-08 12:25:01 +0100

    Added Cronometro.svelte

---
+++ tallerGitSvelte/src/lib/Cronometro.svelte
@@ -0,0 +1,30 @@
+<script>
+    let time = 0;
+    let interval;
+    let isRunning = false;
+
+    function startTimer() {
+        if (isRunning) return;
+        isRunning = true;
+        interval = setInterval(() => {
+            time += 1;
+        }, 1000);
+    }
+    function pauseTimer() {
+        isRunning = false;
+        clearInterval(interval);
+    }
+    function resetTimer() {
+        isRunning = false;
+        clearInterval(interval);
+        time = 0;
+    }
+
+</script>
+<div>
+    <p>Tiempo: {time} segundos</p>
+    <button on:click={startTimer}>Iniciar</button>
+    <button on:click={pauseTimer}>Pausar</button>
+    <button on:click={resetTimer}>Reiniciar</button>
+
+</div>
